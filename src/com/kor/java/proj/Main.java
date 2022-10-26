d51c6ef1b226401c11c2c6bcd59a2fb2004677b7
fbtjdwls04 <fbtjdwls04@gmail.com>
2022-10-26 21:00:39 +0900

    gitignore 다시적용

--- src/com/kor/java/proj/Main.java
+++ src/com/kor/java/proj/Main.java
@@ -7,10 +7,33 @@ public class Main {
 		System.out.println("=== 프로그램 시작 ===");
 		Scanner sc = new Scanner(System.in);
 		
-		System.out.print("명령어를 입력해주세요 : ");
-		String command = sc.nextLine();
-		
-		System.out.printf("입력된 명령어 : %s\n",command);
+		String command;
+		String title;
+		int num = 1;
+		while(true) {
+			System.out.print("명령어를 입력해주세요 : ");
+			command = sc.nextLine();
+			command = command.trim();
+			if(command.length() == 0) {
+				continue;
+			}
+			if(command.equals("system exit")) {
+				break;
+			}
+			else if(command.equals("article write")) {
+				System.out.print("내용 : ");
+				title = sc.nextLine();
+				System.out.printf("%d번 글이 생성되었습니다.",num);
+				num++;
+				
+			}
+			else if(command.equals("article list")) {
+				System.out.println("게시물이 없습니다.");
+			}
+			else {
+				System.out.printf("%s(은)는 존재하지 않는 명령어 입니다.\n",command);
+			}
+		}
 	
 		sc.close();
 		System.out.println("=== 프로그램 끝 ===");
