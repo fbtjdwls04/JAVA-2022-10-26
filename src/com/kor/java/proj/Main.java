3f0bb82153e022c593495fa09d4a95c75da6df39
fbtjdwls04 <fbtjdwls04@gmail.com>
2022-10-28 19:20:41 +0900

    게시판 작성 테스트

--- src/com/kor/java/proj/Main.java
+++ src/com/kor/java/proj/Main.java
@@ -11,59 +11,55 @@ public class Main {
 		String command;
 		String title;
 		String text;
-		
+
 		int num = 0;
 		System.out.println("=== 프로그램 시작 ===");
-		while(true) {
+		while (true) {
 			System.out.print("명령어) ");
 			command = sc.nextLine();
 			command = command.trim();
-			if(command.length() == 0){
+			if (command.length() == 0) {
 				continue;
 			}
-			if(command.equals("exit")) {
+			if (command.equals("exit")) {
 				break;
-			}
-			else if(command.equals("article write")) {
+			} else if (command.equals("article write")) {
 				System.out.print("제목 : ");
 				title = sc.nextLine();
 				System.out.print("내용 : ");
 				text = sc.nextLine();
-				System.out.printf("%d번 게시물이 작성 되었습니다.\n",num+1);
+				System.out.printf("%d번 게시물이 작성 되었습니다.\n", num + 1);
 				num++;
-				게시판 저장 = new 게시판(num,title,text);
+				게시판 저장 = new 게시판(num, title, text);
 				arr.add(저장);
-			}
-			else if(command.equals("article list")) {
-				if(arr.size() == 0) {
+			} else if (command.equals("article list")) {
+				if (arr.size() == 0) {
 					System.out.println("게시물이 없습니다.");
-				}
-				else {
-					for(int i = 0; i < arr.size(); i++) {
-						System.out.printf("===(%s) %s ===\n",arr.get(i).num,arr.get(i).title);
-						System.out.printf("%s\n",arr.get(i).text);
+				} else {
+					for (int i = 0; i < arr.size(); i++) {
+						System.out.printf("===(%s) %s ===\n", arr.get(i).num, arr.get(i).title);
+						System.out.printf("%s\n", arr.get(i).text);
 					}
 				}
+			} else {
+				System.out.printf("%s(은)는 존재하지 않는 명령어 입니다.\n", command);
 			}
-			else {
-				System.out.printf("%s(은)는 존재하지 않는 명령어 입니다.\n",command);
-			}
-			
+
 		}
+		sc.close();
 		System.out.println("=== 프로그램 종료 ===");
-		
+
 	}
 }
 
-class 게시판{
+class 게시판 {
 	int num;
 	String title;
 	String text;
-	게시판(int num, String title, String text){
+
+	게시판(int num, String title, String text) {
 		this.num = num;
 		this.title = title;
 		this.text = text;
 	}
 }
-
-
